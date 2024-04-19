6b9757150cb4491301021d75d721702c32408417
baysingerfam4 <165937206+baysingerfam4@users.noreply.github.com>
2024-04-18 19:11:38 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.Design;
+
+namespace L_6_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //For Loop
+            //Declared our vaiable
+            //int count = 0;
+
+            //{
+                //while (count) ;
+
+           // }
+            for(int i =5; i < 10; i++ )
+            {
+                Console.WriteLine(i);
+            }
+       
+            
+            Console.Write("Please enter a whole number:");
+            int number;
+            bool isValid = false;
+
+            while (!isValid)
+            {
+                string userRespose = Console.ReadLine();
+                if (int.TryParse(userRespose, out number)) ;
+                {
+                    isValid = true;
+                }
+
+                {
+                    Console.WriteLine("Please Enter a valid whole number");
+                }
+
+               // Console.WriteLine("Hello, World!");
+               // int count = 0;
+                //while (count <= 10)
+                //{
+                  //  Console.WriteLine(count);
+               // }
+
+                //Console.WriteLine("This is after our while loop");
+                //{
+
+               // }
+            }
+        }
+    }
+}
